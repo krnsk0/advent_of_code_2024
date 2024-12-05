2fcb52113adf2ca519091b1e337cf02643f61099
Jonathan Kurinsky <jkurinsky@tubi.tv>
2024-12-04 20:24:56 -0500

    day 2

--- day4/solution1.py
+++ day4/solution1.py
@@ -1,8 +1,85 @@
 # https://adventofcode.com/2024/day/4
+import re
 
-useRealInput = False
+def getInput(useRealInput):
+    with open('input_1_real.txt' if useRealInput else 'input_0_test.txt', 'r') as file:
+        input = file.read()
+    return input
 
-with open('input_1_real.txt' if useRealInput else 'input_0_test.txt', 'r') as file:
-    contents = file.read()
+def countXmasInStrs(strs):
+    count = 0
+    for str in strs:
+        count += len(re.findall(r"XMAS", str))
+    return count
+
+def getLeftToRight(input):
+    return input.split('\n')
+
+def getRightToLeft(input):
+    return list(map(lambda x: x[::-1], input.split('\n')))
+
+def getMatrix(input):
+    matrix = list(map(lambda x: list(x), input.split('\n')))
+    # (matrix, width/height)
+    return (matrix, len(matrix))
+
+def getTopToBottom(input):
+    matrix, n = getMatrix(input)
+    out = ['' for _ in range(n)]
+    for y in range(n):
+        for x in range(n):
+            out[x] += matrix[y][x]
+    return out
+
+def getBottomToTop(input):
+    val = getTopToBottom(input)
+    return list(map(lambda x: x[::-1], val))
+
+def getBottomLeftToTopRight(input):
+    matrix, n = getMatrix(input)
+    out = ['' for _ in range(n * 2 - 1)]
+    for y in range(n - 1, -1, -1):
+        for x in range(n):
+            total = x + y
+            out[total] += matrix[y][x]
+    return out
+
+def getTopRightToBottomLeft(input):
+    val = getBottomLeftToTopRight(input)
+    return list(map(lambda x: x[::-1], val))
+
+def getTopLeftToBottomRight(input):
+    # first flip horizontally in place
+    matrix, n = getMatrix(input)
+    for y in range(n // 2):
+        for x in range(n):
+            matrix[y][x], matrix[n - y - 1][x] = matrix[n - y - 1][x], matrix[y][x]
+
+    # then get diagonals
+    out = ['' for _ in range(n * 2 - 1)]
+    for y in range(n - 1, -1, -1):
+        for x in range(n):
+            total = x + y
+            out[total] += matrix[y][x]
+    return out
+
+def getBottomRightToTopLeft(input):
+    val = getTopLeftToBottomRight(input)
+    return list(map(lambda x: x[::-1], val))
+
+def getAll(input):
+    return [
+        *getLeftToRight(input),
+        *getRightToLeft(input),
+        *getTopToBottom(input),
+        *getBottomToTop(input),
+        *getBottomLeftToTopRight(input),
+        *getTopRightToBottomLeft(input),
+        *getTopLeftToBottomRight(input),
+        *getBottomRightToTopLeft(input),
+    ]
 
 
+useRealData = True
+count = countXmasInStrs(getAll(getInput(useRealData)))
+print('part 1 solution', count)
--- day4/solution2.py
+++ day4/solution2.py
@@ -1,8 +1,39 @@
 # https://adventofcode.com/2024/day/4
 
-useRealInput = False
 
-with open('input_1_real.txt' if useRealInput else 'input_0_test.txt', 'r') as file:
-    contents = file.read()
+def getInput(useRealInput):
+    with open('input_1_real.txt' if useRealInput else 'input_0_test.txt', 'r') as file:
+        input = file.read()
+    return input
+
+def asMatrix(input):
+    matrix = list(map(lambda x: list(x), input.split('\n')))
+    return matrix
+
+def getInnerValues(matrix):
+    out = []
+    size = len(matrix)
+    for y in range(size - 2):
+        for x in range(size - 2):
+            out.append((x + 1, y + 1))
+    return out
+
+def isMas(matrix, coord):
+    x, y = coord
+    if matrix[y][x] != 'A': return False
+    val = matrix[y - 1][x - 1] + matrix[y - 1][x + 1] + matrix[y + 1][x - 1] + matrix[y + 1][x + 1]
+    return val in ('MSMS', 'MMSS', 'SMSM', 'SSMM')
 
 
+def getAnswer(input):
+    matrix = asMatrix(input)
+    inner = getInnerValues(matrix)
+    count = 0
+    for coord in inner:
+        if isMas(matrix, coord):
+            count += 1
+    return count
+
+
+useRealInput = True
+print(getAnswer(getInput(useRealInput)))
