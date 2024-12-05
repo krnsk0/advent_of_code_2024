bf1081f5e61649754b00d7603b54e595d49d0802
Jonathan Kurinsky <jkurinsky@tubi.tv>
2024-12-04 20:26:59 -0500

    set up day 5

---
+++ day5/solution2.py
@@ -0,0 +1,7 @@
+# https://adventofcode.com/2024/day/5
+
+def getInput(useRealInput):
+    with open('input_1_real.txt' if useRealInput else 'input_0_test.txt', 'r') as file:
+        input = file.read()
+    return input
+
