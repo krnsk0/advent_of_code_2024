beab17c0ba8876335522936ac3210cc46c302fb0
Jonathan Kurinsky <jkurinsky@tubi.tv>
2024-12-06 06:54:50 -0500

    solve part 1

---
+++ day6/solution1.py
@@ -0,0 +1,68 @@
+# https://adventofcode.com/2024/day/5
+
+def getInput(useRealInput):
+    with open('input_1_real.txt' if useRealInput else 'input_0_test.txt', 'r') as file:
+        input = file.read()
+    return input
+
+def printMatrix(matrix):
+    for row in matrix:
+        print(''.join(row))
+
+def getMatrix(input):
+    matrix = list(map(lambda x: list(x), input.split('\n')))
+    # matrix, width, height
+    return (matrix, len(matrix[0]), len(matrix))
+
+def initialize(matrix, width, height):
+    for y in range(height):
+        for x in range(width):
+            if matrix[y][x] == '^':
+                return (x, y)
+
+def getNextSquare(x, y, dir):
+    nx, ny = x, y
+    if dir == 0:
+        ny -= 1
+    elif dir == 1:
+        nx += 1
+    elif dir == 2:
+        ny += 1
+    else:
+        nx -= 1
+    return nx, ny
+
+def isObstacle(matrix, width, height, x, y):
+    if 0 <= x < width and 0 <= y < height:
+        return matrix[y][x] == '#'
+    else: return False
+
+def solve(input):
+    matrix, width, height = getMatrix(input)
+    x, y = initialize(matrix, width, height)
+    direction = 0
+    seen = set()
+
+    print('INPUT:')
+    printMatrix(matrix)
+    print('')
+
+    while 0 <= x < width and 0 <= y < height:
+        seen.add((x, y))
+        nx, ny = getNextSquare(x, y, direction)
+        blocked = isObstacle(matrix, width, height, nx, ny)
+        if blocked:
+            direction += 1
+            if direction == 4: direction = 0
+        nx, ny = getNextSquare(x, y, direction)
+        matrix[y][x] = 'X'
+        x, y, = nx, ny
+
+    printMatrix(matrix)
+    return len(seen)
+
+
+
+useRealInput = True
+solution = solve(getInput(useRealInput))
+print("part 1 solution", solution)
