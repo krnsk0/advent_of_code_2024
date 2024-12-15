8efda0bfd1c01cd24d6a3934318a1f58ea41f9c8
Jonathan Kurinsky <jkurinsky@tubi.tv>
2024-12-14 20:10:06 -0500

    finish day 9

--- day9/solution2.py
+++ day9/solution2.py
@@ -1,15 +1,87 @@
 # https://adventofcode.com/2024/day/9
-from collections import defaultdict
 
 from _helpers import get_input
 
-def getInput(useRealInput):
-    with open('input_1.txt' if useRealInput else 'input_0.txt', 'r') as file:
-        input = file.read()
-    return input
+
+def parse(input):
+    """
+    will generate list of tuple of type
+    (id, length)
+    id=None means it is free space
+    """
+    out = []
+    for i, n in enumerate(input):
+        if i % 2 == 0:
+            out.append((i // 2, int(n)))
+        else:
+            out.append((None, int(n)))
+    return out
+
+
+def debug(parsed):
+    out = ""
+    for id, size in parsed:
+        if id is None:
+            out += size * "."
+        else:
+            out += str(id) * size
+    print(out)
+
+
+def defrag(parsed):
+    j = len(parsed) - 1
+    while j >= 0:
+        id, size = parsed[j]
+        if id is not None:
+            # print(f"\nlooking for space for file {id} with size {size}")
+            i = 0
+            while i < j:
+                destId, destSize = parsed[i]
+                if destId == None:
+                    # print(f"checking against {i}, size {destSize}")
+                    if destSize == size:
+                        # print(f"found spot at {i}; same size")
+                        parsed[i] = (id, size)
+                        parsed[j] = (None, size)
+                        # debug(parsed)
+                        break
+                    elif size < destSize:
+                        # print(f"found spot at {i}; file is smaller than space")
+                        diff = destSize - size
+                        parsed[j] = (None, size)
+                        parsed[i] = (id, size)
+                        parsed.insert(i + 1, (None, diff))
+                        # debug(parsed)
+                        break
+                i += 1
+        j -= 1
+
+
+def getChecksum(parsed):
+    # id
+    blocks = []
+    for id, size in parsed:
+        for _ in range(size):
+            if id is not None:
+                blocks.append(id)
+            else:
+                blocks.append(0)
+
+    checksum = 0
+    for i, id in enumerate(blocks):
+        checksum += i * id
+
+    return checksum
 
 
 def solve(input):
-    pass
+    parsed = parse(input)
+    print("input:")
+    debug(parsed)
+    defrag(parsed)
+    print("\ndefragged:")
+    debug(parsed)
+    return getChecksum(parsed)
+
 
 print("\npart 1 solution:", solve(get_input(use_real=True)))
