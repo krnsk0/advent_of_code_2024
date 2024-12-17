e88141a160ee5ca5c7cf5ffa0f7c3bafd3c1eab7
Jonathan Kurinsky <jkurinsky@tubi.tv>
2024-12-17 09:03:38 -0500

    initialize more puzzles

---
+++ day17/solution1.py
@@ -0,0 +1,10 @@
+# https://adventofcode.com/2024/day/17
+from _helpers import get_input
+
+
+def solve(input):
+    pass
+
+
+# sample out is 4,6,3,5,6,3,5,2,1,0
+print("\npart 1 solution:", solve(get_input(use_real=False)))
