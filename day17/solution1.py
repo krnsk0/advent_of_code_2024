d2f139c4cedd6ad0a4093b5d484d6c61f3442ad5
Jonathan Kurinsky <jkurinsky@tubi.tv>
2024-12-17 11:06:37 -0500

    parse input

--- day17/solution1.py
+++ day17/solution1.py
@@ -2,8 +2,24 @@
 from _helpers import get_input
 
 
+def parse_input(input):
+    split = input.split("\n")
+    a = int(split[0].split(":")[1])
+    b = int(split[1].split(":")[1])
+    c = int(split[2].split(":")[1])
+    programStr = split[4].split(":")[1]
+    program = [int(x) for x in programStr.split(",")]
+    return a, b, c, program
+
+
+def debug(a, b, c, program):
+    print(f"A: {a}\nB: {b}\nC: {c}")
+    print("Program:", program)
+
+
 def solve(input):
-    pass
+    a, b, c, program = parse_input(input)
+    debug(a, b, c, program)
 
 
 # sample out is 4,6,3,5,6,3,5,2,1,0
